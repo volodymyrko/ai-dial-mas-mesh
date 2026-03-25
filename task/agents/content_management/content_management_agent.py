24664c47782ac524885394bc803ea24d5f60f0ad
Volodymyr Kovtun <volodymyr_kovtun@epam.com>
2026-03-25 23:51:48 +0200

    fix typo

--- task/agents/content_management/content_management_agent.py
+++ task/agents/content_management/content_management_agent.py
@@ -6,7 +6,7 @@ from task.tools.base_tool import BaseTool
 #TODO:
 # Just simply extend the BaseAgent and provide the constructor
 
-class ContentManagementAgent:
+class ContentManagementAgent(BaseAgent):
     def __init__(self, endpoint: str, tools: list[BaseTool]):
         super().__init__(
             endpoint = endpoint,
